66081c0884388ba0a61601a0aed550de6842149c
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-09 10:33:36 +0000

    2023年01月09日 - 01

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,20 @@
 class SudokuSolver {
 
+ /*
+  CHAR 00 - 08 : A (ROW1)
+  CHAR 09 - 17 : B (ROW2)
+  CHAR 18 - 26 : C (ROW3)
+  CHAR 27 - 35 : D (ROW4)
+  CHAR 36 - 44 : E (ROW5)
+  CHAR 45 - 53 : F (ROW6)
+  CHAR 54 - 62 : G (ROW7)
+  CHAR 63 - 71 : H (ROW8)
+  CHAR 72 - 80 : I (ROW9)
+  */
+  
   validate(puzzleString) {
+    const reg = /[1-9\.]{81}/;
+    return reg.test(puzzleString);
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
