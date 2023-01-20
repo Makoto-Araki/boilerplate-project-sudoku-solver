ac88dd29c6eb27a554532a3d98dcbfe52e6e7ac6
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-20 09:11:51 +0000

    2023年01月20日 - 03

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,7 +3,6 @@ class SudokuSolver {
 
   // Costructor
   constructor() {
-    this.maxLoop = 1;
     this.rows = [
       [  0,  1,  2,  3,  4,  5,  6,  7,  8] ,
       [  9, 10, 11, 12, 13, 14, 15, 16, 17] ,
@@ -48,218 +47,42 @@ class SudokuSolver {
     };
   }
 
-  checkRow(puzzleString, row) {
-    //
-  }
-
-  checkCol(puzzleString, col) {
-    //
-  }
-
-  checkReg(puzzleString, reg) {
-    //
-  }
-
-  // Extract array data from main according to arr
-  extract(main, arr) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-      result.push(main[arr[i]]);
-    }
-    return result;
-  }
-
-  // Find data according to row
-  /*
-  findDataRow(arr, pos, val) {
-    let curent = -1;
+  checkRow(idx, val) {
+    let tmp = -1;
     let result = false;
     for (let i = 0; i < this.rows.length; i++) {
-      console.log(`CCC : ${this.rows[i]}`);
-      console.log(`DDD : ${pos}`);
-      console.log(`EEE : ${val}`);
-      if (this.rows[i].indexOf(pos) !== -1) {
-        curent = i;
+      if (this.rows[i].indexOf(idx) !== -1) {
+        tmp = i;
         break;
       }
     }
-    console.log(`FFF : ${curent}`);
-    for (let i = 0; i < this.rows[curent].length; i++) {
-      console.log(`GGG : ${arr[this.rows[curent]]}`);
-      if (arr[this.rows[curent][i]] === val) {
-        result = true;
-        break;
-      }
+    for (let i = 0; i < this.rows[tmp].length; i++) {
+      if (puzzleString)
     }
-    return result;
   }
-  */
-  
-  // Find data according to col
-  /*
-  findDataCol(arr, pos, val) {
-    let curent = -1;
-    let result = false;
-    for (let i = 0; i < this.cols.length; i++) {
-      if (this.cols[i].indexOf(pos) !== -1) {
-        curent = i;
-        break;
-      }
-    }
-    for (let i = 0; i < this.cols[curent].length; i++) {
-      if (arr[this.cols[curent][i]] === val) {
-        result = true;
-        break;
-      }
-    }
-    return result;
+
+  checkCol(val) {
+    //
   }
-  */
-  
+
+  checkReg(val) {
+    //
+  }
+
   // Puzzle Solve Method
   solve(puzzleString) {
-    for (let i = 0; i < this.maxLoop; i++) {
-
-      /*
-        puzzleString =>
-          7.9..5.1.  // 9chars
-          85.4....2  // 9chars
-          432......  // 9chars
-          1...69.83  // 9chars
-          .9.....6.  // 9chars
-          62.71...9  // 9chars
-          ......194  // 9chars
-          5....4.37  // 9chars
-          .4.3..6..  // 9chars
-      */
-      
-      let regArray = [];
-      for (let j = 0; j < this.regs.length; j++) {
-        regArray.push(this.extract(puzzleString, this.regs[j]))
+    for (let i = 0; i < 1; i++) {
+      let arr = [];
+      for (let j = 0; j < 81; j++)
+        arr.push([]);
+      for (let j = 0; j < puzzleString.length; j++) {
+        if (puzzleString[j] === '.') {
+          // 
+        }
       }
-
-      /*
-        regArray =>
-          [
-            [ '.', '.', '9', '8', '5', '.', '4', '3', '2' ],  => tempArray[0]
-            [ '.', '.', '5', '4', '.', '.', '.', '.', '.' ],  => tempArray[1]
-            [ '.', '1', '.', '.', '.', '2', '.', '.', '.' ],  => tempArray[2]
-            [ '1', '.', '.', '.', '9', '.', '6', '2', '.' ],  => tempArray[3]
-            [ '.', '6', '9', '.', '.', '.', '7', '1', '.' ],  => tempArray[4]
-            [ '.', '8', '3', '.', '6', '.', '.', '.', '9' ],  => tempArray[5]
-            [ '.', '.', '.', '5', '.', '.', '.', '4', '.' ],  => tempArray[6]
-            [ '.', '.', '.', '.', '.', '4', '3', '.', '.' ],  => tempArray[7]
-            [ '1', '9', '4', '.', '3', '7', '6', '.', '.' ],  => tempArray[8]
-          ]
-      */
-
-      for (let j = 0; j < regArray.length; j++) {
-        let curentReg = j;
-        let tempArray = regArray[i];
-        let voidArray = [];
-        let lackArray = [];
-
-        for (let k = 0; k < this.regs[curentReg].length; k++) {
-          if (puzzleString[this.regs[curentReg][k]] === '.') {
-            voidArray.push(this.regs[curentReg][k]);
-          }
-        }
-
-        for (let k = 1; k <= 9; k++) {
-          if (tempArray.indexOf(k.toString()) === -1) {
-            lackArray.push(k.toString());
-          }
-        }
-
-        /*
-          curentReg => 0
-          tempArray => [ '.', '.', '9', '8', '5', '.', '4', '3', '2' ]
-          voidArray => [  0 ,  1 ,  11 ]
-          lackArray => [ '1', '6', '7' ]
-        */
-
-        // voidArray => [  0 ,  1 ,  11 ]
-        for (let k = 0; k < voidArray.length; k++) {
-          let tmp1 = -1;
-          let tmp2 = -1;
-          let flg1 = false;
-          let flg2 = false;
-          let count = 0;
-          let index = [];
-          let value = [];
-
-          // lackArray => [ '1', '6', '7' ]
-          for (let l = 0; l < lackArray.length; l++) {
-
-            /*
-              this.rows[0] => [  0,  1,  2,  3,  4,  5,  6,  7,  8 ]
-              this.rows[1] => [  9, 10, 11, 12, 13, 14, 15, 16, 17 ]
-              this.rows[2] => [ 18, 19, 20, 21, 22, 23, 24, 25, 26 ]
-              this.rows[3] => [ 27, 28, 29, 30, 31, 32, 33, 34, 35 ]
-              this.rows[4] => [ 36, 37, 38, 39, 40, 41, 42, 43, 44 ]
-              this.rows[5] => [ 45, 46, 47, 48, 49, 50, 51, 52, 53 ]
-              this.rows[6] => [ 54, 55, 56, 57, 58, 59, 60, 61, 62 ]
-              this.rows[7] => [ 63, 64, 65, 66, 67, 68, 69, 70, 71 ]
-              this.rows[8] => [ 72, 73, 74, 75, 76, 77, 78, 79, 80 ]
-            */
-
-            for (let m = 0; m < this.rows.length; m++) {
-              if (this.rows[m].indexOf(voidArray[k]) !== -1) {
-                tmp1 = m;
-              }
-            }
-
-            for (let m = 0; m < this.rows[tmp1].length; m++) {
-              if (puzzleString[this.rows[tmp1][m]] === lackArray[l]) {
-                flg1 = true;
-              }
-            }
-
-            /*
-              this.cols[0] => [  0,  9, 18, 27, 36, 45, 54, 63, 72 ]
-              this.cols[1] => [  1, 10, 19, 28, 37, 46, 55, 64, 73 ]
-              this.cols[2] => [  2, 11, 20, 29, 38, 47, 56, 65, 74 ]
-              this.cols[3] => [  3, 12, 21, 30, 39, 48, 57, 66, 75 ]
-              this.cols[4] => [  4, 13, 22, 31, 40, 49, 58, 67, 76 ]
-              this.cols[5] => [  5, 14, 23, 32, 41, 50, 59, 68, 77 ]
-              this.cols[6] => [  6, 15, 24, 33, 42, 51, 60, 69, 78 ]
-              this.cols[7] => [  7, 16, 25, 34, 43, 52, 61, 70, 79 ]
-              this.cols[8] => [  8, 17, 26, 35, 44, 53, 62, 71, 80 ]
-            */
-
-            for (let m = 0; m < this.cols.length; m++) {
-              if (this.cols[m].indexOf(voidArray[k]) !== -1) {
-                tmp2 = m;
-              }
-            }
-
-            for (let m = 0; m < this.cols[tmp2].length; m++) {
-              if (puzzleString[this.cols[tmp2][m]] === lackArray[l]) {
-                flg2 = true;
-              }
-            }
-
-            if (flg1 === false && flg2 === false) {
-              count = count + 1;
-              index.push(voidArray[k]);
-              value.push(lackArray[l]);
-            }
-            
-            console.log(`A01 : ${count}`);
-          }
-          
-          if (count === 1) {
-            puzzleString[index[0]] = value[0];
-          }
-        }
-        
-        console.log(`A02 : ${puzzleString}`);
-      }
-      
     }
-    //console.log(`AFTER  : ${result}`);
-    //return result;
   }
+  
 }
 
 module.exports = SudokuSolver;
